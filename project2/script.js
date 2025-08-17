50c8332147474105a89baedbf353c09c958932bb
gyanaranjan-das <dasgyanaranjan835@gmail.com>
2025-08-17 15:55:14 +0530

    first commit

---
+++ project2/script.js
@@ -0,0 +1,25 @@
+//constants for tag options
+
+const tagOptions = [
+    "p","h1","h2",
+    "h3","h4","h5",
+    "h6",'span',
+];
+
+// Get DOM elements
+
+const optionsContainer = document.querySelector(".options");
+const outputContainer = document.querySelector(".output");
+const tagsSelect = document.getElementById("tags");
+const paragraphsSlider = document.getElementById("paragraphs");
+
+const wordslider = document.getElementById("words");
+
+const paragraphsValue = document.getElementById("paragraphValue");
+
+const wordsValues = document.getElementById("wordValue");
+
+
+function createOptionsUI(){
+    
+}
